5c60888cee678b7c1d55407da66722442a0c82e7
wanderlust23 <alpamyshovanazima@gmail.com>
2023-03-15 14:44:47 -0500

    adding one more class

--- src/test/java/pages/Amazon.java
+++ src/test/java/pages/Amazon.java
@@ -4,6 +4,6 @@ public class Amazon {
     public static void main(String[] args) {
         System.out.println("Welcome to Amazon!");
         System.out.println("Hey, How are you doing?");
-        System.out.println("Hellooooo, it meeee!");
+        System.out.println("hahahhahaha");
     }
 }
