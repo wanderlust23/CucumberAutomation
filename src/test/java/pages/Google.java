fde63d0c13057e1c0b1f797b0b058ed392f7f6b5
wanderlust23 <alpamyshovanazima@gmail.com>
2023-03-15 12:52:51 -0500

    adding google page

---
+++ src/test/java/pages/Google.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class Google {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Google!");
+        System.out.println("Hey, bro! how are you doing?");
+    }
+}
